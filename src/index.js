f0214dcbe9688f875150ba4c7c0caaffbdda49bf
Vitali Kuntsevich <kvadesstudio@gmail.com>
2023-09-15 12:42:27 +0300

    fix: debug code

--- src/index.js
+++ src/index.js
@@ -1,34 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-
-  const OPEN_BRACKETS = ['(', '{'];
-  const BRACKETS_PAIR = {
-    [')']: '(',
-    ['}']: '{',
-  };
-
-  function isBracketsOk(str) {
-    let stack = [];
-
-    for (let i = 0; i < str.length; i++) {
-      let currentSymbol = str[i];
-
-      if (OPEN_BRACKETS.includes(currentSymbol)) {
-        stack.push(currentSymbol);
-      } else {
-        if (stack.length === 0) {
-          return false;
-        }
-
-        let topElement = stack[stack.length - 1];
-
-        if (BRACKETS_PAIR[currentSymbol] === topElement) {
-          stack.pop();
-        } else {
-          return false;
-        }
-      }
-    }
-    return stack.length === 0;
+  let open_brackets = [];
+  let brackets_pair = [];
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    open_brackets.push(bracketsConfig[i][0]);
   }
-  console.log(check - ('()', [['(', ')']]) - isBracketsOk('()', [['(', ')']]));
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    brackets_pair.push(bracketsConfig[i][1]);
+  }
+  function OpenBrackets(str) {
+    return open_brackets.includes(str);
+  }
+  function CloseBrackets(str) {
+    return brackets_pair.includes(str);
+  }
+  function BracketsOk(str, str2) {
+    let topElement = open_brackets.indexOf(str);
+    let currentSymbol = brackets_pair.indexOf(str2);
+    if (topElement == currentSymbol) return true;
+    return false;
+  }
+  for (let i = 0; i < str.length - 1; i++) {
+    if (OpenBrackets(str[i]) && CloseBrackets(str[i + 1]) && BracketsOk(str[i], str[i + 1])) {
+      str = str.replace(str[i] + str[i + 1], "");
+      i = -1;
+    }
+  }
+  if (str.length == 0) return true;
+  return false;
 }
